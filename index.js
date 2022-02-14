6252f00bceb834b8cc9f2172214655e5f67150dd
Trigun Joshi <iamtrigun@gmail.com>
2022-02-14 15:09:45 -0800

    validation and direction func added for desc

--- index.js
+++ index.js
@@ -15,14 +15,9 @@ app.use('/users', userRoutes);
 
 const url = 'https://api.hatchways.io/assessment/blog/posts';
 
-// app.get("/:id", (req, res) => {
-//   console.log(req.params);
-//   res.send('I am here');
-//   res.end();
-// })
 
 app.get("/", (req, res) => {
-  res.send('Try different tags: For instance; /politics, /tech')
+  res.status(200).json('Try different tags: For instance; /politics, /tech')
 })
 
 
@@ -33,7 +28,7 @@ app.get("/:tag", (req, res) => {
   request(myUrl, function (error, response, body) {
 
     //console.log('body:', JSON.parse(body));
-    res.send(JSON.parse(body))
+    res.status(200).send(JSON.parse(body))
   });
 
 })
@@ -41,35 +36,54 @@ app.get("/:tag", (req, res) => {
 app.get("/api/posts", async (req, res) => {
   const temp = req.query.tags;
   console.log("temp:", temp);
+  if (!temp) {
+    return res.status(400).send({
+      "error": "Tags paramter is required"
+    })
+  }
+  const sortBy = req.query.sortBy;
+  if (sortBy && ((sortBy !== "likes") && (sortBy !== "authorId") && (sortBy !== "id") && (sortBy !== "popularity") && (sortBy !== "reads"))) {
+    return res.status(400).send({
+      "error": "sortBy paramter is invalid"
+    })
+  }
+  const direction = req.query.direction;
+  if (direction && (direction !== "asc" && direction !== "desc")) {
+    return res.status(400).send({
+      "error": "direction parameter is invalid"
+    })
+  }
   const tags = temp.split(',');
   let posts = [];
-
 
   const getPosts = (cat) => {
     return new Promise((resolve, reject) => {
       const myUrl = `${url}?tag=${cat}`;
-      console.log("myUrl", myUrl)
+      console.log("myUrl", myUrl);
+
       request(myUrl, function (error, response, body) {
         posts = posts.concat(JSON.parse(body).posts)
         resolve()
         //console.log('body:', JSON.parse(body).posts);
 
-      });
+      })
     })
   }
 
   for (let t of tags) {
     await getPosts(t);
   }
-  const sortBy = req.query.sortBy;
-  posts.sort((first, second) => first[sortBy] - second[sortBy])
+
+
+
+  posts.sort((first, second) => direction === 'asc' ? first[sortBy] - second[sortBy] : second[sortBy] - first[sortBy])
   res.send(posts);
 })
 
 
 app.get('*', (req, res) => {
   res.status(404);
-  res.send('This is not the page you are looking for')
+  res.send("Tags parameter is required")
 })
 
 
